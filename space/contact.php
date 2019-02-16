112dcf186e761122b5877d4ae86992b92d2075bd
GoonTurtle <truttle12@gmail.com>
2019-02-16 11:44:08 -0500

    Moved things to content page

--- space/contact.php
+++ space/contact.php
@@ -13,9 +13,7 @@
 	<div id="content">
 	<span id="goon">Contact</span>
 	<span class="cal_goon">Team 3604</span>
-	<br>
-	<H2>Contact at: whs.robotics.3604@gmail.com</H2>
-If you would like to contact the Goon Squad, please send us an email! We will get back to you as soon as we can! If you do not receive a response within 72 hours, please just send another email.
+	<?php include("content/contact.php")?>
 	</div>
 	<?php include("php/footer.php")?>
 	</body>
---
+++ space/content/sponsors.php
@@ -0,0 +1,25 @@
+</td><td>Visit our great sponsors!</td></tr></table><br><br>
+		 <a href="http://gm.com"><img src="img/gm.png" height="64px"></a>&nbsp &nbsp &nbsp
+		 <a href="http://ford.com"><img src="img/ford.png" height="64px"></a>&nbsp &nbsp &nbsp
+		<a href="http://mcdonalds.com"><img src="img/mcdonalds.png" height="64px"></a>&nbsp &nbsp &nbsp
+		<a href="http://tedsonindustries.com"><img src="img/tedson.jpg" height="64px"></a>&nbsp &nbsp &nbsp
+		<a href="http://icrservices.com"><img src="img/icr.png" height="64px"></a>&nbsp &nbsp &nbsp
+		<a href="http://rcoeng.com"><img src="img/rco.png" height="64px"></a>&nbsp &nbsp &nbsp
+		<a href="http://searchplusinternational.com/"><img src="img/searchplus.png" height="64px"></a>&nbsp &nbsp &nbsp
+		<!--<a <img src="img/dadara.jpg" height="128px">&nbsp &nbsp &nbsp-->
+		<a href="http://www.local387.org/"><img src="img/uaw.png" height="64px"></a>&nbsp &nbsp &nbsp
+		<a href="http://region1a.uaw.org/local600/"><img src="img/uaw2.png" height="64px"></a>&nbsp &nbsp &nbsp
+		<a href="http://wbsd.co"><img src="img/wbsd.png" height="64px"></a>&nbsp &nbsp &nbsp
+		<img src="img/dadara.png" height="64px">&nbsp &nbsp &nbsp
+		<a href="https://www.texasroadhouse.com/" style="text-decoration: none;"><img src="img/texasRoadhouse.png" height="64px"></a>&nbsp &nbsp &nbsp
+		<a href="http://www.nvidia.com/content/global/global.php"><img src="img/nvidia-logo.png" height="64px"></a>&nbsp &nbsp &nbsp
+		<a href="http://bestbuy.com/"><img src="img/bestbuy.png" height="64px"></a>&nbsp &nbsp &nbsp
+		<a href="http://www.valiantcorp.com/"><img src="img/valiant.png" height="64px"></a>&nbsp &nbsp &nbsp
+		<a href="https://www.fcagroup.com/en-US/Pages/home.aspx"><img src="img/fca.png" height="64px"></a>&nbsp &nbsp &nbsp
+		<span style="font-size: 64px; font-weight: bold;">IICC</span>
+		<br>
+		<a href="https://megs.mde.state.mi.us/megs/" style="color: white; text-decoration: none; font-family: Sans-serif;">
+		<img src="img/michigan.png" height="64px">&nbsp &nbsp &nbsp
+		<br>
+		<span style="color:black">Michigan Educational Grant</a><br>
+		</a>
