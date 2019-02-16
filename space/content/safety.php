fda9bde782727b8605adff9cb95927f10fccf3ac
Lordoftheflame <anthonydalfonso15@gmail.com>
2019-02-16 16:55:53 -0500

    added more to safety pg

--- space/content/safety.php
+++ space/content/safety.php
@@ -5,8 +5,13 @@
 		how to use it all. Additionally, everyone on the team is trained, and certified in CPR (cardiopulmonary resuscitation), and AED 
 		(Automated External Defibrillator) in case someone ever becomes unresponsive, or passes out. Our safety captains insure our safety 
 		by providing us with evacuation plans in case of a emergency and treat simple cuts, and bruises that we may get in the shop to
-		make sure they do not get infected later. We have two safety captains; one being a veteran with 3 years of past experience, and 
-		the other being a rookie with it being their first year. Our lead safety captain won three URL safety stars in last years game two
+		make sure they do not get infected later. We have three safety captains; one being a veteran with 3 years of past experience, and two
+		 being a rookies with it being their first year. Our lead safety captain won three URL safety stars in last years game two
 		at qualifying events and one at worlds! The safety captains keep a log of all of the past injuries in our shop, to prove how safe
 		we actually are. In conclusion, safety is one of the main priorities in our shop, and that is thanks to our great safety captains.
 	</p>
+	<h2>Our Safety Captains!</h2>
+	<br>
+	<img class="SafetyP" src="img/safetyN"></img>
+	<img class="SafetyP" src="img/safetyA"></img>
+	<img class="SafetyP" src="img/safetyK"></img>
