dfde761fa5a4adbf47d5086523ef80649d612377
Lordoftheflame <anthonydalfonso15@gmail.com>
2019-04-19 14:44:27 -0400

    update to website

--- space/content/news.php
+++ space/content/news.php
@@ -1,10 +1,23 @@
-
 <br><br>
+<h2>We Won States!</h2>
+<br>
+<h3>4/10/2019 - 4/13/2019</h3>
+<br>
+<p>We competed at States in Saginaw at SVSU (Saginaw Valley State University). We played on Ford Field, where we placed 15th on the field. We were on the 3rd Alliance with the alliance captains The Gems team 4362 and Lightning Robotics team 862. Our alliance was able to win Ford Field, which was hard in itself, but we also went on to the State Championship. We played 5 matches. Luckily for us though, we won, and are now State Champions! Who are we? Goon Squad!</p>
+<br><br>
+<img class="RobotP" src="img/Abotstates.JPG"></img>
+<h2> We Competed at Jackson </h2>
+<br>
+<h3>3/22/2019 - 3/23/2019</h3>
+<br>
+<p>We completed our week four competition at Jackson. We had a fun time and fought well to seed as the 5th place out of 40 teams. We managed to earn 28 ranking pints with an average of 2.33 points! Our team was the 4th alliance captain at the event and made it to semifinals, sadly we lost and did not receive the blue banner.In conclusion we had an amazing time at our event and look Forward to states.</p>
+<br>
+<img class="RobotP" src="img/Jackson.jpg"></img>
 <h2>We Competed at Gibraltar </h2>
 <br>
 <h3>3/1/2019 - 3/2/2019</h3>
 <br>
-<p>We completed our week one competition at Gibraltar. We played well seating 2nd out 40 teams overall we 29 ranking points with a average of 2.4 ranking points. Our team made it all the way to the finals, but sadly we lost and did not receive a blue banner. Overall we had a great time and look forward to our next event at Jackson.</p>
+<p>We completed our week one competition at Gibraltar. We played well seating 2nd out 40 teams. overall we earned 29 ranking points with a average of 2.4 ranking points. Our team made it all the way to the finals, but sadly we lost and did not receive a blue banner. Overall we had a great time and look forward to our next event at Jackson.</p>
 <br>
 <img class="RobotP" src="img/abotclimb.jpg"></img>
 <h2>We completed A-bot!</h2>
@@ -15,4 +28,5 @@
 <img class="RobotP" src="img/SpaceAbot.jpg"></img>
 <br>
 <br>
+<h2>Legacy News</h2>
 <p>If you want to read the legacy news page, you can do so <a href="legacynews.php">here</a>.</p>
